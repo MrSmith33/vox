05b82171de67911364aab86d7ae6b8bf63414e85
Andrey Penechko <mrsmith33@yandex.ru>
2017-05-17 15:27:13 +0300

    Wrap MemAddress into BytePtr, WordPtr, DwordPtr and QwordPtr.

--- amd64asm.d
+++ amd64asm.d
@@ -11,8 +11,6 @@ enum Reg32 : ubyte {EAX,ECX,EDX,EBX,ESP,EBP,ESI,EDI,R8D,R9D,R10D,R11D,R12D,R13D,
 enum Reg64 : ubyte {RAX,RCX,RDX,RBX,RSP,RBP,RSI,RDI,R8, R9, R10, R11, R12, R13, R14, R15 }
 enum bool isAnyReg(R) = is(R == Reg64) || is(R == Reg32) || is(R == Reg16) || is(R == Reg8);
 
-import std.meta : staticIndexOf, AliasSeq;
-enum sizeofReg(R) = [1, 2, 4, 8][staticIndexOf!(R, AliasSeq!(Reg8, Reg16, Reg32, Reg64))];
 enum Reg64Max = cast(Reg64)(Reg64.max+1);
 enum Reg32Max = cast(Reg32)(Reg32.max+1);
 enum Reg16Max = cast(Reg16)(Reg16.max+1);
@@ -23,10 +21,10 @@ bool regNeedsRexPrefix(Reg8 reg) { return reg >= 4; }
 
 
 import std.string : format;
-struct Imm8  { ubyte  value; alias Reg = Reg8 ; string toString(){ return format("0X%02X", value); } }
-struct Imm16 { ushort value; alias Reg = Reg16; string toString(){ return format("0X%02X", value); } }
-struct Imm32 { uint   value; alias Reg = Reg32; string toString(){ return format("0X%02X", value); } }
-struct Imm64 { ulong  value; alias Reg = Reg64; string toString(){ return format("0X%02X", value); } }
+struct Imm8  { ubyte  value; alias RegT = Reg8 ; string toString(){ return format("0X%02X", value); } }
+struct Imm16 { ushort value; alias RegT = Reg16; string toString(){ return format("0X%02X", value); } }
+struct Imm32 { uint   value; alias RegT = Reg32; string toString(){ return format("0X%02X", value); } }
+struct Imm64 { ulong  value; alias RegT = Reg64; string toString(){ return format("0X%02X", value); } }
 enum bool isAnyImm(I) = is(I == Imm64) || is(I == Imm32) || is(I == Imm16) || is(I == Imm8);
 
 
@@ -121,21 +119,28 @@ struct MemAddress {
 }
 
 MemAddress memAddrDisp32(uint disp32) {
-	return MemAddress(MemAddrType.disp32, Reg32.ESP, Reg32.EBP, SibScale(), disp32); } // 0b00
+	return MemAddress(MemAddrType.disp32, Reg32.ESP, Reg32.EBP, SibScale(), disp32); }
 MemAddress memAddrIndexDisp32(Reg32 indexReg, SibScale scale, uint disp32) {
-	return MemAddress(MemAddrType.indexDisp32, indexReg, Reg32.EBP, scale, disp32); } // 0b00
+	return MemAddress(MemAddrType.indexDisp32, indexReg, Reg32.EBP, scale, disp32); }
 MemAddress memAddrBase(Reg32 baseReg) {
 	return MemAddress(MemAddrType.base, Reg32.ESP, baseReg); }
 MemAddress memAddrBaseDisp32(Reg32 baseReg, uint disp32) {
-	return MemAddress(MemAddrType.baseDisp32, Reg32.ESP, baseReg, SibScale(), disp32); } // 0b10
+	return MemAddress(MemAddrType.baseDisp32, Reg32.ESP, baseReg, SibScale(), disp32); }
 MemAddress memAddrBaseIndex(Reg32 baseReg, Reg32 indexReg, SibScale scale) {
 	return MemAddress(MemAddrType.baseIndex, indexReg, baseReg, scale); }
 MemAddress memAddrBaseIndexDisp32(Reg32 baseReg, Reg32 indexReg, SibScale scale, uint disp32) {
-	return MemAddress(MemAddrType.baseIndexDisp32, indexReg, baseReg, scale, disp32); } // 0b10
+	return MemAddress(MemAddrType.baseIndexDisp32, indexReg, baseReg, scale, disp32); }
 MemAddress memAddrBaseDisp8(Reg32 baseReg, ubyte disp8) {
-	return MemAddress(MemAddrType.baseDisp8, Reg32.ESP, baseReg, SibScale(), disp8); } // 0b01
+	return MemAddress(MemAddrType.baseDisp8, Reg32.ESP, baseReg, SibScale(), disp8); }
 MemAddress memAddrBaseIndexDisp8(Reg32 baseReg, Reg32 indexReg, SibScale scale, ubyte disp8) {
-	return MemAddress(MemAddrType.baseIndexDisp8, indexReg, baseReg, scale, disp8); } // 0b01
+	return MemAddress(MemAddrType.baseIndexDisp8, indexReg, baseReg, scale, disp8); }
+
+struct BytePtr  { MemAddress addr; alias RegT = Reg8 ; alias addr this; }
+struct WordPtr  { MemAddress addr; alias RegT = Reg16; alias addr this; }
+struct DwordPtr { MemAddress addr; alias RegT = Reg32; alias addr this; }
+struct QwordPtr { MemAddress addr; alias RegT = Reg64; alias addr this; }
+enum bool isAnyPtr(M) = is(M == BytePtr) || is(M == WordPtr) || is(M == DwordPtr) || is(M == QwordPtr);
+
 
 // Sink defines put(T) for ubyte, ubyte[], Imm8, Imm16, Imm32, Imm64
 struct CodeGen_x86_64(Sink)
@@ -165,13 +170,14 @@ struct CodeGen_x86_64(Sink)
 		sink.put(encodeModRegRmByte(ModRmMod(0b11), src_reg, dst_rm));          // ModR/M
 	}
 	private void putInstrRegImm(R, I)(ubyte opcode, R dst_rm, I src_imm) if (isAnyReg!R && isAnyImm!I) {
-		static assert(sizeofReg!R == I.value.sizeof, "Sizes of imm and reg must be equal");
+		static assert(is(R == I.RegT), "Sizes of imm and reg must be equal");
 		static if (is(R == Reg16)) sink.put(LegacyPrefix.OPERAND_SIZE);         // 16 bit operand prefix
 		putRexByte_B(dst_rm);                                                   // REX
 		sink.put(opcode | (dst_rm & 0b0111));                                   // Opcode
 		sink.put(src_imm);                                                      // Imm
 	}
-	private void putInstrRegMem(R)(ubyte opcode, R dst_r, MemAddress src_mem) if (isAnyReg!R) {
+	private void putInstrRegMem(R, M)(ubyte opcode, R dst_r, M src_mem) if (isAnyReg!R && isAnyPtr!M) {
+		static assert(is(R == M.RegT), "Sizes of reg and ptr must be equal");
 		static if (is(R == Reg16)) sink.put(LegacyPrefix.OPERAND_SIZE);         // 16 bit operand prefix
 		putRexByte_RXB(dst_r, cast(R)src_mem.indexReg, cast(R)src_mem.baseReg); // REX
 		sink.put(opcode);                                                       // Opcode
@@ -182,9 +188,12 @@ struct CodeGen_x86_64(Sink)
 		else if (src_mem.hasDisp8)
 			sink.put(src_mem.disp8);                                            // disp8
 	}
-	private void putInstrMemImm(I)(ubyte opcode, MemAddress dst_mem, I src_imm) if (isAnyImm!I) {
-		static if (is(I.Reg == Reg16)) sink.put(LegacyPrefix.OPERAND_SIZE);     // 16 bit operand prefix
-		putRexByte_RXB(cast(I.Reg)0, cast(I.Reg)dst_mem.indexReg, cast(I.Reg)dst_mem.baseReg);// REX
+	private void putInstrMemImm(M, I)(ubyte opcode, M dst_mem, I src_imm) if (isAnyPtr!M && isAnyImm!I) {
+		static assert( // allow special case of QwordPtr and Imm32
+			is(M.RegT == I.RegT) || ( is(M == QwordPtr) && is(I == Imm32) ),
+			"Sizes of ptr and imm must be equal");
+		static if (is(M == WordPtr)) sink.put(LegacyPrefix.OPERAND_SIZE);     // 16 bit operand prefix
+		putRexByte_RXB(cast(M.RegT)0, cast(M.RegT)dst_mem.indexReg, cast(M.RegT)dst_mem.baseReg);// REX
 		sink.put(opcode);                                                       // Opcode
 		sink.put(dst_mem.modRmByte);                                            // ModR/M
 		sink.put(dst_mem.sibByte);                                              // SIB
@@ -192,14 +201,7 @@ struct CodeGen_x86_64(Sink)
 			sink.put(Imm32(dst_mem.disp32));                                    // disp32
 		else if (dst_mem.hasDisp8)
 			sink.put(dst_mem.disp8);                                            // disp8
-
-		static if (is(I == Imm64)) {
-			Imm32 imm32 = Imm32(cast(uint)src_imm.value);
-			assert(src_imm.value == imm32.value, "Imm64 must only contain 32-bit values");
-			sink.put(imm32);                                                      // Mem64 Imm32
-		} else {
-			sink.put(src_imm);                                                  // Mem8/16/32 Imm8/16/32
-		}
+		sink.put(src_imm);                                                      // Mem8/16/32 Imm8/16/32
 	}
 
 	void beginFunction() {
@@ -221,21 +223,20 @@ struct CodeGen_x86_64(Sink)
 	void mov(Reg32 dst_rm, Imm32 src_imm) { putInstrRegImm(0xB8, dst_rm, src_imm); }
 	void mov(Reg64 dst_rm, Imm64 src_imm) { putInstrRegImm(0xB8, dst_rm, src_imm); }
 
-	void mov(Reg8  dst_reg, MemAddress src_rm) { putInstrRegMem(0x8A, dst_reg, src_rm); }
-	void mov(Reg16 dst_reg, MemAddress src_rm) { putInstrRegMem(0x8B, dst_reg, src_rm); }
-	void mov(Reg32 dst_reg, MemAddress src_rm) { putInstrRegMem(0x8B, dst_reg, src_rm); }
-	void mov(Reg64 dst_reg, MemAddress src_rm) { putInstrRegMem(0x8B, dst_reg, src_rm); }
+	void mov(Reg8  dst_reg, BytePtr  src_rm) { putInstrRegMem(0x8A, dst_reg, src_rm); }
+	void mov(Reg16 dst_reg, WordPtr  src_rm) { putInstrRegMem(0x8B, dst_reg, src_rm); }
+	void mov(Reg32 dst_reg, DwordPtr src_rm) { putInstrRegMem(0x8B, dst_reg, src_rm); }
+	void mov(Reg64 dst_reg, QwordPtr src_rm) { putInstrRegMem(0x8B, dst_reg, src_rm); }
 
-	void mov(MemAddress dst_rm, Reg8  src_reg) { putInstrRegMem(0x88, src_reg, dst_rm); }
-	void mov(MemAddress dst_rm, Reg16 src_reg) { putInstrRegMem(0x89, src_reg, dst_rm); }
-	void mov(MemAddress dst_rm, Reg32 src_reg) { putInstrRegMem(0x89, src_reg, dst_rm); }
-	void mov(MemAddress dst_rm, Reg64 src_reg) { putInstrRegMem(0x89, src_reg, dst_rm); }
+	void mov(BytePtr  dst_rm, Reg8  src_reg) { putInstrRegMem(0x88, src_reg, dst_rm); }
+	void mov(WordPtr  dst_rm, Reg16 src_reg) { putInstrRegMem(0x89, src_reg, dst_rm); }
+	void mov(DwordPtr dst_rm, Reg32 src_reg) { putInstrRegMem(0x89, src_reg, dst_rm); }
+	void mov(QwordPtr dst_rm, Reg64 src_reg) { putInstrRegMem(0x89, src_reg, dst_rm); }
 
-	void mov(MemAddress dst_rm, Imm8  src_imm) { putInstrMemImm(0xC6, dst_rm, src_imm); }
-	void mov(MemAddress dst_rm, Imm16 src_imm) { putInstrMemImm(0xC7, dst_rm, src_imm); }
-	void mov(MemAddress dst_rm, Imm32 src_imm) { putInstrMemImm(0xC7, dst_rm, src_imm); }
-	// Imm64 can only use 32bit values here, it is sign extended to 64-bits
-	void mov(MemAddress dst_rm, Imm64 src_imm) { putInstrMemImm(0xC7, dst_rm, src_imm); }
+	void mov(BytePtr  dst_rm, Imm8  src_imm) { putInstrMemImm(0xC6, dst_rm, src_imm); }
+	void mov(WordPtr  dst_rm, Imm16 src_imm) { putInstrMemImm(0xC7, dst_rm, src_imm); }
+	void mov(DwordPtr dst_rm, Imm32 src_imm) { putInstrMemImm(0xC7, dst_rm, src_imm); }
+	void mov(QwordPtr dst_rm, Imm32 src_imm) { putInstrMemImm(0xC7, dst_rm, src_imm); }
 
 	void ret() { sink.put(0xC3); }
 	//void ret(uint retValue) { sink.put(0xC3); }
